02c8092d6311f6d0285228fd035f6dee86425a91
Pramod Yadav <yadavpramod441@gmail.com>
2021-05-16 21:03:44 +0530

    Update india.py

--- Navigation/india.py
+++ Navigation/india.py
@@ -11,24 +11,28 @@ def test():
     India.replace('Total','India',inplace=True)
     India_Selected_Column=India[['State','Confirmed','Recovered','Deaths','Active']]
     State=st.selectbox("Select State",India[['State']])
+    Vaccine=pd.read_csv("http://api.covid19india.org/csv/latest/cowin_vaccine_data_statewise.csv")
 
-    col1,col2 = st.beta_columns((1,1))
+    #col1,col2 = st.beta_columns((1,1))
 
     ### Column 1
-    col1.header("Cumulative")
+    """    col1.header("Cumulative")
 
-    IndianStatesDaily=pd.read_csv("https://api.covid19india.org/csv/latest/states.csv")
-    col1.plotly_chart(px.line(data_frame=IndianStatesDaily[IndianStatesDaily['State']==State],x='Date',y='Confirmed',title=State))
-    col1.plotly_chart(px.line(data_frame=IndianStatesDaily[IndianStatesDaily['State']==State],x='Date',y='Recovered',title=State))
-    col1.plotly_chart(px.line(data_frame=IndianStatesDaily[IndianStatesDaily['State']==State],x='Date',y='Tested',title=State))
-    col1.plotly_chart(px.line(data_frame=IndianStatesDaily[IndianStatesDaily['State']==State],x='Date',y='Deceased',title=State))
+        IndianStatesDaily=pd.read_csv("https://api.covid19india.org/csv/latest/states.csv")
+        col1.plotly_chart(px.line(data_frame=IndianStatesDaily[IndianStatesDaily['State']==State],x='Date',y='Confirmed',title=State))
+        col1.plotly_chart(px.line(data_frame=IndianStatesDaily[IndianStatesDaily['State']==State],x='Date',y='Recovered',title=State))
+        col1.plotly_chart(px.line(data_frame=IndianStatesDaily[IndianStatesDaily['State']==State],x='Date',y='Tested',title=State))
+        col1.plotly_chart(px.line(data_frame=IndianStatesDaily[IndianStatesDaily['State']==State],x='Date',y='Deceased',title=State))
 
-
+    """
 
     ### Column 2
-    col2.header("Daily")
+    #st.header("Daily")
     IndianStatesDaily=pd.read_csv("https://api.covid19india.org/csv/latest/states.csv")
-    col2.plotly_chart(px.bar(data_frame=IndianStatesDaily[IndianStatesDaily['State']==State],x='Date',y='Confirmed',title=State))
-    col2.plotly_chart(px.bar(data_frame=IndianStatesDaily[IndianStatesDaily['State']==State],x='Date',y='Recovered',title=State))
-    col2.plotly_chart(px.bar(data_frame=IndianStatesDaily[IndianStatesDaily['State']==State],x='Date',y='Tested',title=State))
-    col2.plotly_chart(px.bar(data_frame=IndianStatesDaily[IndianStatesDaily['State']==State],x='Date',y='Deceased',title=State))
+    st.plotly_chart(px.bar(data_frame=IndianStatesDaily[IndianStatesDaily['State']==State],x='Date',y='Confirmed',title="Confirmed Cases so far in " + State,color_discrete_sequence=["blue"] ))
+    st.plotly_chart(px.bar(data_frame=IndianStatesDaily[IndianStatesDaily['State']==State],x='Date',y='Recovered',title="Recovered Cases so far in " +State ,color_discrete_sequence=["green"]))
+    st.plotly_chart(px.bar(data_frame=IndianStatesDaily[IndianStatesDaily['State']==State],x='Date',y='Tested',title="Tested so far in " + State, color_discrete_sequence=["magenta"]))
+    st.plotly_chart(px.bar(data_frame=IndianStatesDaily[IndianStatesDaily['State']==State],x='Date',y='Deceased',title="Deceased so far in" + State,color_discrete_sequence=["red"]))
+    st.plotly_chart(px.bar(data_frame=Vaccine[Vaccine['State']==State],x='Updated On',y='Total Individuals Vaccinated',title="Vaccinated so far in" + State,color_discrete_sequence=["goldenrod"]))
+
+
