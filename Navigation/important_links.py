de13cfe0217a07c542c4c5f8eb3b54d607cc0429
Pramod Yadav <yadavpramod441@gmail.com>
2021-05-16 21:03:28 +0530

    Create important_links.py

---
+++ Navigation/important_links.py
@@ -0,0 +1,15 @@
+import streamlit as st
+
+def test():
+    st.title("Important Links For India Only")
+    st.write("The objective of this page is to help people gain access to vital resources by sharing information only. However, we request the beneficiaries to use their discretion and verify the leads on their own before taking any action. If you find inaccurate information or any lead engaging in illegal practices, kindly inform us. We will take it down as soon as possible.")
+    st.write("1. Life Coronasafe Network: https://life.coronasafe.network ")
+    st.write("2. Covid Resources India: https://www.covidresourcesindia.com ")
+    st.write("3. Covid Beds India: https://covidbedsindia.in ")
+    st.write("4. Covid Resource Database: https://linktr.ee/Okayival ")
+    st.write("5. Covid Fight Club: https://covidfightclub.org ")
+    st.write("6. Twitter Covid Fighter : https://twitter.com/CovidFightersI1 ")
+    
+    
+    
+    
