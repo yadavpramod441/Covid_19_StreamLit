12f451e237824e2374c50be9afd83554280c4396
Pramod Yadav <yadavpramod441@gmail.com>
2021-05-16 21:03:05 +0530

    Create about.py

---
+++ Navigation/about.py
@@ -0,0 +1,9 @@
+import streamlit as st
+
+def test():
+    st.title("About Us")
+    st.write("This project is built as Academic project for my final year's Master(I.T.) program. The dashboard is to track the Covid-19 Situation around the Globe. Dedicately we have created a page for **India** because recently we have seen surge in the second wave cases. For this project we have used 2 different data source to built the dashboard.")
+    st.write("1.  https://github.com/owid/covid-19-data/tree/master/public/data \n 2.  https://api.covid19india.org/csv/latest/state_wise.csv ")
+    
+    
+    
